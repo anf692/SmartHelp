0b40e17179b5241763ef18ba0886aedadeee1a1c
anf692 <fassane348@gmail.com>
2026-08-04 18:12:04 +0000

    fix:Ajout des corsheader pour autoriser l'application frontend de communiquer avce le back

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.support_ticket import router as router_support_ticket
 import os
 
 app = FastAPI(title="SmartHelp API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Créer le dossier temporaire s'il n'existe pas
 os.makedirs("temp_files", exist_ok=True)
